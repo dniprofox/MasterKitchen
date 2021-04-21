d4c944529c949dd861b92c0861112f8575ebdcb6
FoxWeb <dneprfox@gmail.com>
2021-04-21 20:54:14 +0300

    changes

--- docs/js/main.js
+++ docs/js/main.js
@@ -7,6 +7,17 @@ $(function () {
     $('.mobile-menu').toggle(400);   
   });  
   
+  // modal screen activate
+$('.mobile-menu').live('click', function(){
+  modalScreen.show();
+  $('body').css('overflow','hidden');
+  });
+  
+  // modal screen de-activate
+  $('body, html').live('click', function(){
+  modalScreen.hide();
+  $('body').css('overflow','auto');
+  });
 
    
  
