70decafa581c7be4845804d3a849a247f4dea013
FoxWeb <dneprfox@gmail.com>
2021-04-21 12:56:53 +0300

    ready

---
+++ docs/js/main.js
@@ -0,0 +1,19 @@
+$(function () {  
+
+
+  ////////menu
+	$('.menu__btn').on("click", function(){
+    $(this).toggleClass('open');
+    $('.mobile-menu').toggle(400);   
+  });  
+  
+
+   
+ 
+  //  var mixer = mixitup('.products__inner-box');
+
+  
+
+
+  
+});
