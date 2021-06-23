584267ac3eba1826af834ad99f2ac58ab80409fd
FoxWeb <dneprfox@gmail.com>
2021-06-23 21:34:33 +0300

    changes

--- docs/js/main.js
+++ docs/js/main.js
@@ -4,10 +4,24 @@ $(function () {
   ////////menu
 	$('.menu__btn,.mobail__header-prev').on("click", function(){
     $(this).toggleClass('open');
-    $('.mobile-menu').toggle(400);   
-    // $('body').toggle('lock');
+    $('.mobile-menu').toggle(400);  
+    
   });  
   
+   // ToTop////
+   $(window).on('scroll',function () {
+      
+    if ($(this).scrollTop() != 0)
+       $('#toTop').fadeIn("slow");
+    else
+       $('#toTop').fadeOut("slow");
+   });
+   
+    $('#toTop').on('click',function () {
+        $('body,html').animate({
+        scrollTop: 0
+        }, 800);
+    });    
  
 
    
